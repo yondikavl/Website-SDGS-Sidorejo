be2facb886ef72fe2c707b2feec8e35431f215e5
yondikavl <yondikaviolanda@gmail.com>
2023-12-05 10:48:59 +0700

    update 05/12/23 10:48

--- src/app/tentang/page.jsx
+++ src/app/tentang/page.jsx
@@ -27,7 +27,7 @@ const page = () => {
           </p>
         </article>
       </section>
-      <section className="flex flex-wrap md:gap-4 justify-between py-8">
+      <section className="flex flex-wrap md:gap-4 justify-center py-8">
         {Array.from({ length: 18 }, (_, index) => (
           <Cardsdgs key={index} image={`/sdgs/${index + 1}.jpg`} />
         ))}
